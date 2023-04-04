79f41743c20e637165bde72248440242aa5119da
palmtopj <palmtopj@uw.edu>
2023-04-03 22:26:23 -0700

    Timer starts and stops at zero

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 console.log("tacos");
+var timerElement = document.querySelector(".timerCount");
 var startQuizButton = document.querySelector("#startButton");
-var timerElement = document.querySelector(".timer-count");
 
 var timer;
 var timerCount;
+// startQuizButton.addEventListener("click", function (event) {
+//   console.log(event);
+// });
+startQuizButton.addEventListener("click", startQuiz);
 
 function startQuiz() {
-  startTimer();
+  timerCount = 20;
+  console.log("startQuiz");
+  timer = setInterval(function () {
+    timerCount--;
+    timerElement.textContent = timerCount;
+    if (timerCount === 0) {
+      clearInterval(timer);
+    }
+  }, 1000);
 }
-
-startQuizButton.addEventListener("click", function (event) {
-  console.log(event);
-});
