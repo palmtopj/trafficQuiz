f17ca8372f96dc72cca60548aa21d20347e89094
palmtopj <palmtopj@uw.edu>
2023-04-03 21:51:42 -0700

    adding script.js

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 console.log("tacos");
 var startQuizButton = document.querySelector("#startButton");
+var timerElement = document.querySelector(".timer-count");
+
+var timer;
+var timerCount;
+
+function startQuiz() {
+  startTimer();
+}
 
 startQuizButton.addEventListener("click", function (event) {
   console.log(event);
